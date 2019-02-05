8a0c6aada44a6f74536cd9b11a3e1ec9fe340a82
dauie <rlutt@student.42.us.org>
2019-02-05 13:19:56 -0800

    refactored some of the code to be a bit more managable, still having some issues getting the scan to trigger properly >.<

--- goJam.go
+++ goJam.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/mdlayher/genetlink"
-	"github.com/mdlayher/netlink"
-	"github.com/remyoudompheng/go-netlink/nl80211"
 	"log"
 	"os"
-)
 
+	"github.com/mdlayher/genetlink"
+	"github.com/mdlayher/netlink"
+	"github.com/mdlayher/wifi"
+	"github.com/remyoudompheng/go-netlink/nl80211"
+)
 
 type Station struct {
 	BSSID string
@@ -21,45 +23,108 @@ func help() {
 	os.Exit(1)
 }
 
-func triggerScan(conn *genetlink.Conn, fam *genetlink.Family) error {
-	req := genetlink.Message{
-		Header: genetlink.Header{
+func triggerScan(conn *genetlink.Conn, fam *genetlink.Family, iface *wifi.Interface) error {
+	encoder := netlink.NewAttributeEncoder()
+	encoder.Uint32(nl80211.ATTR_IFINDEX, uint32(iface.Index))
+	ifaceAttrib, err := encoder.Encode()
+	if err != nil {
+		log.Panicln(err)
+	}
+	req := genetlink.Message {
+		Header: genetlink.Header {
 			Command: nl80211.CMD_TRIGGER_SCAN,
 			Version: fam.Version,
 		},
+		Data: ifaceAttrib,
 	}
-	flags := netlink.HeaderFlagsRequest | netlink.HeaderFlagsDump
-	_, err := conn.Execute(req, fam.ID, flags)
+	flags := netlink.HeaderFlagsRequest | netlink.HeaderFlagsAcknowledge
+	msgs, err := conn.Execute(req, fam.ID, flags)
 	if err != nil {
-		log.Panicln("genetlink.Conn.Execute()", err)
+		log.Panicln("genetlink.Conn.Execute", err)
+	}
+	for _, v := range msgs {
+		fmt.Println(v)
 	}
 	return nil
 }
 
 func getScanResults(conn *genetlink.Conn, fam *genetlink.Family) []genetlink.Message {
-	req := genetlink.Message{
-		Header: genetlink.Header{
+	req := genetlink.Message {
+		Header: genetlink.Header {
 			Command: nl80211.CMD_GET_SCAN,
 			Version: fam.Version,
 		},
 	}
 	flags := netlink.HeaderFlagsRequest | netlink.HeaderFlagsDump
-	stations, err := conn.Execute(req, fam.ID, flags)
+	msgs, err := conn.Execute(req, fam.ID, flags)
 	if err != nil {
 		log.Panicln("genetlink.Conn.Execute()", err)
 	}
-	return stations
+	for _, v := range msgs {
+		fmt.Println(v)
+	}
+	return msgs
 }
 
-func parseScanResults() {
+func getInterface(targetIface string) (* wifi.Interface, error) {
+	client, err := wifi.New()
+	var ret *wifi.Interface = nil
 
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer func(){
+		if err := client.Close(); err != nil {
+			log.Panicln(err)
+		}
+	}()
+	ifaces, err := client.Interfaces()
+	if err != nil {
+		log.Panicln(err)
+	}
+	for _, v := range ifaces {
+		if v.Name == targetIface {
+			ret = v
+		}
+	}
+	if ret == nil {
+		return nil, fmt.Errorf("interface %s not found", targetIface)
+	}
+	return ret, nil
+}
+
+func getNL80211ScanMCID(fam *genetlink.Family) (uint32, error) {
+	scanMCID := uint32(0)
+	for _, v := range fam.Groups {
+		if v.Name == "scan" {
+			scanMCID = v.ID
+		}
+	}
+	if scanMCID == 0 {
+		return 0, errors.New("could not find nl80211 'scan' multicast ID")
+	}
+	return scanMCID, nil
+}
+
+func getNL80211Family(conn *genetlink.Conn) (* genetlink.Family, error) {
+	fam, err := conn.GetFamily("nl80211")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, errors.New("nl80211 not found on system" + err.Error())
+		}
+		return nil, err
+	}
+	return &fam, nil
 }
 
 
 func main() {
-
 	if len(os.Args) < 3 {
 		help()
+	}
+	iface, err := getInterface(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
 	}
 	conn, err := genetlink.Dial(nil)
 	if err != nil {
@@ -70,30 +135,18 @@ func main() {
 			log.Fatalln("genetlink.Conn.Close()", err)
 		}
 	}()
-	fam, err := conn.GetFamily("nl80211")
+	fam, err := getNL80211Family(conn)
 	if err != nil {
-		if os.IsNotExist(err) {
-			log.Fatal("nl80211 not found on system", err)
-		}
-		log.Fatalln("genetlink.Conn.GetFamily('nl80211')", err)
+		log.Fatalln(err)
 	}
-	scanMCID := uint32(0)
-	for _,v := range fam.Groups {
-		fmt.Println(v)
-		if v.Name == "scan" {
-			scanMCID = v.ID
-		}
-	}
-	if scanMCID == 0 {
-		log.Fatalln("could not find nl80211 'scan' multicast ID")
-	}
+	scanMCID, err := getNL80211ScanMCID(fam)
 	if err := conn.JoinGroup(scanMCID); err != nil {
 		log.Fatalln("genetlink.Conn.JoinGroup()", err)
 	}
-	if err := triggerScan(conn, &fam); err != nil {
+	if err := triggerScan(conn, fam, iface); err != nil {
 		log.Fatalln(err)
 	}
-	stations := getScanResults(conn, &fam)
+	stations := getScanResults(conn, fam)
 	for _, v := range stations {
 		fmt.Println(v)
 	}
